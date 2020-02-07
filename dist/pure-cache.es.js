aa7261f415e19031b2158bb5b2bd9b9608d8c7ce
Ganapati V S <vsg.inbox@gmail.com>
2020-02-08 02:25:04 +0530

    Dependency update

--- dist/pure-cache.es.js
+++ dist/pure-cache.es.js
@@ -37,6 +37,40 @@ function _defineProperty(obj, key, value) {
   return obj;
 }
 
+function ownKeys(object, enumerableOnly) {
+  var keys = Object.keys(object);
+
+  if (Object.getOwnPropertySymbols) {
+    var symbols = Object.getOwnPropertySymbols(object);
+    if (enumerableOnly) symbols = symbols.filter(function (sym) {
+      return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+    });
+    keys.push.apply(keys, symbols);
+  }
+
+  return keys;
+}
+
+function _objectSpread2(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i] != null ? arguments[i] : {};
+
+    if (i % 2) {
+      ownKeys(Object(source), true).forEach(function (key) {
+        _defineProperty(target, key, source[key]);
+      });
+    } else if (Object.getOwnPropertyDescriptors) {
+      Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+    } else {
+      ownKeys(Object(source)).forEach(function (key) {
+        Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+      });
+    }
+  }
+
+  return target;
+}
+
 /**
  * Events list
  */
@@ -104,7 +138,7 @@ function () {
     });
 
     // Configuration
-    this.config = Object.assign({}, inMemoryDefaultConfig, config); // Expirer queue
+    this.config = _objectSpread2({}, inMemoryDefaultConfig, {}, config); // Expirer queue
 
     this.queue = {}; // Instance dispose status
 
@@ -229,7 +263,7 @@ function () {
     _classCallCheck(this, PureCache);
 
     // Configuration
-    this.config = Object.assign({}, pureCacheDefaultConfig, config); // Event listeners
+    this.config = _objectSpread2({}, pureCacheDefaultConfig, {}, config); // Event listeners
 
     var _mitt = mitt(),
         on = _mitt.on,
